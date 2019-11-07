8de3788111d3461c6bc752d7da16e9dbf9fbc74c
Mayank Kulkarni <31683839+mayankk245@users.noreply.github.com>
2019-11-07 18:36:05 +0530

    home.css
    css file hass been created for home.js

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Axios from "axios";
-
-class Home extends React.Component{
-  constructor(props){
+import "./css/home.css";
+class Home extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      user:'',
+    this.state = {
+      user: ""
     };
   }
-  render(){
-return(
-<div>
-<p>{this.props.name}</p>
-<p>{this.state.user}</p>
-</div>
-);
+  render() {
+    return (
+      <div class="name">
+        <p>{this.props.name}</p>
+        <p>{this.state.user}</p>
+      </div>
+    );
   }
 
-  componentWillMount(){
-  
-    this.setState({user:'Kulkarni'})
+  componentWillMount() {
+    this.setState({ user: "Kulkarni" });
   }
 }
 
