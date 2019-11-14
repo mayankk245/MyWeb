4099bec748a07cf3a999e73f1f36055334ec436e
Mayank Kulkarni <31683839+mayankk245@users.noreply.github.com>
2019-11-14 15:52:19 +0530

    react sky
    react sky dependency has been added and implemented

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./css/home.css";
-import Typical from 'react-typical';
+import Typical from "react-typical";
+import Sky from "react-sky";
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -11,18 +13,40 @@ class Home extends React.Component {
   render() {
     return (
       <div class="container">
+        <Sky
+          images={{
+            0: "https://cdn4.iconfinder.com/data/icons/whsr-january-flaticon-set/512/rocket.png",
+            1: "https://images.vexels.com/media/users/3/157970/isolated/preview/c156b4270aea292b9b335dd463ea17eb-earth-planet-icon-earth-icon-by-vexels.png",
+            2: "https://www.pngrepo.com/png/125875/170/satellite.png",
+            3: "https://vignette.wikia.nocookie.net/samorost6178/images/9/99/Meteor.png/revision/latest?cb=20170128122740"
+          }}
+          how={30}
+          time={100}
+          size={"70px"}
+          background={"black"}
+        />
         <div class="menu">
           <p> Menu </p>
         </div>
         <div class="intro">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTxEOVTcvf8wEI1bGPVZrCVADRvWGpFsVzrowss8S1SuQl_xJjK" />
           <p>Mayank Kulkarni</p>
-          <div id="type"><Typical
-        steps={["I'm a" , 1000, "I'm a Developer", 1000,"I'm a Avid Learner", 1000,"I'm a Freelancer", 1000]}
-        loop={Infinity}
-        wrapper="d"
-      /></div>
-    
+          <div id="type">
+            <Typical
+              steps={[
+                "I'm a",
+                1000,
+                "I'm a Developer",
+                1000,
+                "I'm a Avid Learner",
+                1000,
+                "I'm a Freelancer",
+                1000
+              ]}
+              loop={Infinity}
+              wrapper="d"
+            />
+          </div>
         </div>
         <div class="bottom1">
           <p>></p>
