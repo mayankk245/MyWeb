e4245a2b74d6a1a13de0340453606c7d46dc340b
Mayank Kulkarni <31683839+mayankk245@users.noreply.github.com>
2019-11-14 11:44:42 +0530

    Typing effect added
    Updated code

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Axios from "axios";
 import "./css/home.css";
+import Typical from 'react-typical';
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -17,10 +17,15 @@ class Home extends React.Component {
         <div class="intro">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTxEOVTcvf8wEI1bGPVZrCVADRvWGpFsVzrowss8S1SuQl_xJjK" />
           <p>Mayank Kulkarni</p>
-          <p>I am a</p>
+          <div id="type"><Typical
+        steps={["I'm a" , 1000, "I'm a Developer", 1000,"I'm a Avid Learner", 1000,"I'm a Freelancer", 1000]}
+        loop={Infinity}
+        wrapper="d"
+      /></div>
+    
         </div>
         <div class="bottom1">
-          <p>Down</p>
+          <p>></p>
         </div>
       </div>
     );
